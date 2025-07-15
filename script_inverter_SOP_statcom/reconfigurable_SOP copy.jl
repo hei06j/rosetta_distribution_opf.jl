8def8cf5dd4c5cd690af6823be869a125d6c6ba2
Rahmat Heidari <heidari.rahmat@gmail.com>
2025-07-15 16:44:27 +1000

    script updated

--- script_inverter_SOP_statcom/reconfigurable_SOP copy.jl
+++ script_inverter_SOP_statcom/reconfigurable_SOP copy.jl
@@ -8,7 +8,7 @@ using AppleAccelerate
 using Ipopt
 using Juniper
 using HiGHS
-using Gurobi
+# using Gurobi
 using JuMP
 using LinearAlgebra
 import LinearAlgebra: diag, diagm
@@ -24,16 +24,16 @@ const IM = InfrastructureModels
 # ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level"=>0, "sb"=>"yes","warm_start_init_point"=>"yes", "hsllib"=>HSL_jll.libhsl_path, "linear_solver"=>"ma86")
 ipopt_solver = JuMP.optimizer_with_attributes(Ipopt.Optimizer, "print_level"=>0, "sb"=>"yes","warm_start_init_point"=>"yes")
 highs_solver = optimizer_with_attributes(HiGHS.Optimizer, "output_flag" => false)
-gurobi_solver = optimizer_with_attributes(Gurobi.Optimizer, "output_flag" => false)
+# gurobi_solver = optimizer_with_attributes(Gurobi.Optimizer, "output_flag" => false)
 juniper_solver = optimizer_with_attributes(Juniper.Optimizer, "nl_solver"=>ipopt_solver, "mip_solver" => highs_solver)
 
 data_path = "./data/ENWL_4w_Network1_Feeders1and2/Master.dss"
 
-data_path = "./data/feeder_12/Master.dss"
+# data_path = "./data/feeder_12/Master.dss"
 
 # data_path = "./data/ENWL_4w_Network1_Feeder1/Master.dss"
 
-# data_path = "./data/ENWL_4w_Network1_Feeders1and2_grounded/Master.dss"
+# data_path = "./data/ENWL_4w_Network1_Feeders1and2 copy/Master.dss"
 
 ##
 ### parse data
@@ -43,19 +43,27 @@ data_eng["voltage_source"]["source"]["rs"] *= 0
 data_eng["voltage_source"]["source"]["xs"] *= 0
 data_math = PMD.transform_data_model(data_eng, kron_reduce=false, phase_project=false)
 
+# txbus = [parse(Int,i) for (i,bus) in data_math["bus"] if occursin("tx1", bus["name"])][1]
 sourcebus = [parse(Int,i) for (i,bus) in data_math["bus"] if occursin("sourcebus", bus["name"])][1]
-# sourcebus = [parse(Int,i) for (i,bus) in data_math["bus"] if occursin("tx1", bus["name"])][1]
-sourcebus_branches = [i for (i, branch) in data_math["branch"] if branch["f_bus"]==sourcebus]
+data_math["bus"]["$sourcebus"]["vm"] = [1.0918, 1.0445, 1.0445, 0, 0]
+data_math["bus"]["$sourcebus"]["va"] = [0, -121.511, 121.511, 0, 0] .* pi/180
+data_math["bus"]["$sourcebus"]["vmin"] = copy(data_math["bus"]["$sourcebus"]["vm"])
+data_math["bus"]["$sourcebus"]["vmax"] = copy(data_math["bus"]["$sourcebus"]["vm"])
 
-source2branch = "657"
 new_bus_id = 10000
 data_math["bus"]["$new_bus_id"] = deepcopy(data_math["bus"]["$sourcebus"])
 data_math["bus"]["$new_bus_id"]["bus_i"] = new_bus_id
 data_math["bus"]["$new_bus_id"]["index"] = new_bus_id
 data_math["bus"]["$new_bus_id"]["source_id"] = "bus.sourcebus2"
 data_math["bus"]["$new_bus_id"]["name"] = "sourcebus_2"
+data_math["bus"]["$new_bus_id"]["vm"] = [1.02, 1.02, 1.02, 0, 0]
+data_math["bus"]["$new_bus_id"]["va"] = [0, -120, 120, 0, 0] .* pi/180
+data_math["bus"]["$new_bus_id"]["vmin"] = copy(data_math["bus"]["$new_bus_id"]["vm"])
+data_math["bus"]["$new_bus_id"]["vmax"] = copy(data_math["bus"]["$new_bus_id"]["vm"])
 
-data_math["branch"]["657"]["f_bus"] = new_bus_id
+sourcebus_branches = [i for (i, branch) in data_math["branch"] if branch["f_bus"]==sourcebus]
+source2branch = sourcebus_branches[1]
+data_math["branch"]["$source2branch"]["f_bus"] = new_bus_id
 
 data_math["gen"]["2"] = deepcopy(data_math["gen"]["1"])
 data_math["gen"]["2"]["gen_bus"] = new_bus_id
@@ -64,17 +72,6 @@ data_math["gen"]["2"]["index"] = 2
 
 ##
 function make_case(data_math; combined=true)
-    # sourcebus = [parse(Int,i) for (i,bus) in data_math["bus"] if occursin("sourcebus", bus["name"])][1]
-    # data_math["bus"]["$sourcebus"]["vm"] = [1.0918, 1.0445, 1.0445, 0, 0]
-    # data_math["bus"]["$sourcebus"]["vmin"] = copy(data_math["bus"]["$sourcebus"]["vm"])
-    # data_math["bus"]["$sourcebus"]["vmax"] = copy(data_math["bus"]["$sourcebus"]["vm"])
-    # data_math["bus"]["$sourcebus"]["va"] = [0, -121.511, 121.511, 0, 0] .* pi/180
-
-    # for (i, load) in data_math["load"]
-    #     load["pd"] = [1]
-    # end
-
-    
     sbase = data_math["settings"]["sbase"]                          # p.u.
     sbace_factor = data_math["settings"]["power_scale_factor"]      # 
     vbase = [v for v in values(data_math["settings"]["vbases_default"])][1]
@@ -115,7 +112,8 @@ end
 
 ### ##################### No SOP #####################
 setting = Dict("conventional"=>true, "reconfigurable" => false, "ideal" => false, "dc_link" => true, "induction_motor" => false)
-data_math_no_sop, IM1_bus, IM2_bus, IM3_bus = make_case(deepcopy(data_math); combined=true)
+data_math_no_sop = deepcopy(data_math)
+data_math_no_sop, IM1_bus, IM2_bus, IM3_bus = make_case(data_math_no_sop; combined=true)
 PMD.add_start_vrvi!(data_math_no_sop)
 # model = PMD.instantiate_mc_model(data_math, PMD.IVRENPowerModel, PMD.build_mc_opf);
 model = PMD.instantiate_mc_model(data_math_no_sop, PMD.IVRENPowerModel, RPMD.build_mc_opf_mx_sop; setting=setting);
@@ -136,9 +134,12 @@ IM3_bus_seq = abs.(RPMD.sequence(result["solution"]["bus"]["$IM3_bus"]["vr"][1:3
 
 ## ##################### Conventional SOP #####################
 setting = Dict("conventional"=>true, "reconfigurable" => false, "ideal" => false, "dc_link" => true, "induction_motor" => true)
-data_math_sop, IM1_bus, IM2_bus, IM3_bus = make_case(deepcopy(data_math); combined=true)
-fbus = [parse(Int,i) for (i,bus) in data_math_sop["bus"] if occursin("f1_882", bus["name"])][1] #"F1_882.1.2.3.4"
-tbus = [parse(Int,i) for (i,bus) in data_math_sop["bus"] if occursin("f2_396", bus["name"])][1] #"F2_396.1.2.3.4"
+data_math_sop = deepcopy(data_math)
+data_math_sop, IM1_bus, IM2_bus, IM3_bus = make_case(data_math_sop; combined=true)
+fbus = [parse(Int,i) for (i,bus) in data_math_sop["bus"] if occursin("1_882", bus["name"])][1] #"F1_882.1.2.3.4"
+tbus = [parse(Int,i) for (i,bus) in data_math_sop["bus"] if occursin("2_396", bus["name"])][1] #"F2_396.1.2.3.4"
+# data_math_sop["bus"]["$fbus"]["grounded"][4] = true
+# data_math_sop["bus"]["$tbus"]["grounded"][4] = true
 RPMD.add_sop_inverter_losses_v2!(data_math_sop, fbus, tbus; c_rating_a=20*ones(3), dc_link=setting["dc_link"])
 
 ###
@@ -149,7 +150,7 @@ result_conv = PMD.optimize_model!(model_sop, optimizer=ipopt_solver)
 ## inspect results 
 IMbus_vmneg = [result_conv["solution"]["bus"]["$bus_id"]["vmneg"] for bus_id in [IM1_bus, IM2_bus, IM3_bus]] .*100
 
-IMbus_vmneg_nosop = [IM1_bus_seq[3] ; IM2_bus_seq[3] ; IM3_bus_seq[3]]./100
+IMbus_vmneg_nosop = [IM1_bus_seq[3] ; IM2_bus_seq[3] ; IM3_bus_seq[3]]
 
 ##
 using Plots
